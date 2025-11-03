6033bc9fd09349228a5f1288319916c444eb5c36
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2025-11-03 16:20:42 +0100

    Delete Code directory

--- Code/utils/__pycache__/__init__.py
+++
@@ -1,8 +0,0 @@
-# utils/__init__.py
-from .general_utils import (
-    describe_qaoa,
-    demonstrate_scaling,
-    decode_solution,
-    compute_energy_from_bitstring,
-    repair_with_marginals
-)
--- Code/visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-# visualization/__init__.py
-from .plot_utils import (
-    plot_optimization,
-    plot_alpha_helix_wheel
-)
